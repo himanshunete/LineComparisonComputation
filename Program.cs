e694113c9dce024f99c2624be8022a88fa9ba677
himanshunete <himanshuneteh@gmail.com>
2021-02-02 19:12:55 +0530

    [himanshunete]Add MessageIsPrinted

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LineComparisonComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Coordinates
+            double x1 = Console.Read();
+            double y1 = Console.Read();
+            double x2 = Console.Read();
+            double y2 = Console.Read();
+            double squareX = (x2 - x1) * (x2 - x1);
+            double squareY = (y2 - y1) * (y2 - y1);
+            Console.WriteLine( $" Length of a Line: { Math.Sqrt(squareX + squareY) }" );
+        }
+    }
+}
