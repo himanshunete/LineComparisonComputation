a361b05c2d7e311ef79bfdc1986fcdee06dc412f
himanshunete <himanshuneteh@gmail.com>
2021-02-02 19:20:00 +0530

    [himanshunete]Add MessageIsPrinted

--- Program.cs
+++ Program.cs
@@ -6,14 +6,8 @@ namespace LineComparisonComputation
     {
         static void Main(string[] args)
         {
-            //Coordinates
-            double x1 = Console.Read();
-            double y1 = Console.Read();
-            double x2 = Console.Read();
-            double y2 = Console.Read();
-            double squareX = (x2 - x1) * (x2 - x1);
-            double squareY = (y2 - y1) * (y2 - y1);
-            Console.WriteLine( $" Length of a Line: { Math.Sqrt(squareX + squareY) }" );
+
+            Console.WriteLine(" Welcome to Line Computation Program ");
         }
     }
 }
